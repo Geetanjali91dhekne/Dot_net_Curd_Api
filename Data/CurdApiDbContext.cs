3b994879399efdecd85b8c262d1b2a0be2098566
Geetanjali91dhekne <108217522+Geetanjali91dhekne@users.noreply.github.com>
2023-12-25 15:03:38 +0530

    curd api 3

---
+++ Data/CurdApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CurdApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurdApi.Data
+{
+    public class CurdApiDbContext : DbContext
+    {
+        public CurdApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
