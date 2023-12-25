39ec4cf3385813bd713924a005e8492b1d3a0b8c
Geetanjali91dhekne <108217522+Geetanjali91dhekne@users.noreply.github.com>
2023-12-25 16:35:20 +0530

    Web apis

---
+++ Model/Contacts.cs
@@ -0,0 +1,28 @@
+﻿namespace WebAPIs.Model
+{
+    public class Contacts
+    {
+        public Guid id
+        {
+            get;
+            set;
+        }
+        public string name
+        {
+            get;
+            set;
+        }
+        public string email
+        {
+            get;set;
+        }
+        public string phone
+        {
+            get;set;
+        }
+        public string address
+        {
+            get;set;
+        }
+    }
+}
---
+++ data/Contacts.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIs.Model;
+
+namespace WebAPIs.data
+{
+    public class ContactsAPIDbContext : DbContext
+    {
+        public ContactsAPIDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Contacts> Contacts
+        {
+            get;
+            set;
+        }
+    }
+}
